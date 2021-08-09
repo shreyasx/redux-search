4caeab11fb27a1467bc38a1b1562c8e82e093edd
Shreyas <shreyxs@gmail.com>
2021-08-09 17:55:23 +0530

    DONE

--- src/reducers.js
+++ src/reducers.js
@@ -32,7 +32,7 @@ export const reducer = (state = initialState, action = {}) => {
 				...state,
 				query: action.payload.toLowerCase(),
 				current: 1,
-				items: [...state.items].filter(movie =>
+				items: [...page1, ...page2, ...page3].filter(movie =>
 					movie.name.toLowerCase().includes(action.payload.toLowerCase())
 				),
 			};
