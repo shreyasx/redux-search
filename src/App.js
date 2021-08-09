9e1ddf0ad54c4b3a518380aa59b76025666e723c
Shreyas <shreyxs@gmail.com>
2021-08-09 17:18:34 +0530

    saving progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import React, { Suspense } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "./components/card";
+import * as actions from "./actions";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./style.css";
-const p1 = React.lazy(() => import("./components/p1"));
-const p2 = React.lazy(() => import("./components/p2"));
-const p3 = React.lazy(() => import("./components/p3"));
 
 const App = () => {
+	const page1 = useSelector(state => state.page1);
+	const page2 = useSelector(state => state.page2);
+	const page3 = useSelector(state => state.page3);
+	const [items, setItems] = React.useState(page1);
+	const [current, setCurrent] = React.useState(1);
+	const dispatch = useDispatch();
+
+	const fetchData = () => {
+		if (current === 1)
+			setTimeout(() => {
+				setCurrent(2);
+				setItems([...page1, ...page2]);
+			}, 1000);
+
+		if (current === 2)
+			setTimeout(() => {
+				setCurrent(3);
+				setItems([...page1, ...page2, ...page3]);
+			}, 1000);
+	};
+
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Suspense fallback={null}>
-					<Route path="/" exact component={() => <Redirect to="/page-1" />} />
-					<Route path="/page-1" exact component={p1} />
-					<Route path="/page-2" exact component={p2} />
-					<Route path="/page-3" exact component={p3} />
-				</Suspense>
-			</Switch>
-		</BrowserRouter>
+		<>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "space-evenly",
+				}}
+			>
+				<input
+					type="search"
+					placeholder="Type to search..."
+					name="search"
+					id="searchbox"
+					onChange={event => dispatch(actions.searchPage1(event.target.value))}
+				/>
+			</div>
+			<InfiniteScroll
+				dataLength={items.length}
+				next={fetchData}
+				hasMore={current < 3}
+				loader={<h4>Loading...</h4>}
+				endMessage={
+					<p style={{ textAlign: "center" }}>
+						<b>Yay! You have seen it all.</b>
+					</p>
+				}
+			>
+				<div className="cardlist">
+					{items.map((movie, index) => (
+						<Card movie={movie} key={index} />
+					))}
+				</div>
+			</InfiniteScroll>
+		</>
 	);
 };
 
